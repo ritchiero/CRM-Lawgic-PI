89d1a878125176bfa87668d1c5ed5db2332e08ae
ritchiero <appletreemx@gmail.com>
2026-07-21 20:46:51 -0700

    Actualiza actividad verificada (3801 representantes)

--- src/data/representativesData.ts
+++ src/data/representativesData.ts
@@ -26,7 +26,7 @@ export const representativesData: { name: string; brandCount: number; rank: numb
   { rank: 25, name: "Victor Ramirez Landeros", brandCount: 5921 },
   { rank: 26, name: "Jose Dario Cosio Izcapa", brandCount: 1602 },
   { rank: 27, name: "Paulina Barroso Avila", brandCount: 2792 },
-  { rank: 28, name: "Humberto Mendoza Gonzalez", brandCount: 1233 },
+  { rank: 28, name: "Humberto Mendoza Gonzalez", brandCount: 1278 },
   { rank: 29, name: "Angelica Lara Maldonado", brandCount: 1636 },
   { rank: 30, name: "Gilberto Martinez Maldonado", brandCount: 9467 },
   { rank: 31, name: "Bernardo Herrerias Franco", brandCount: 11899 },
