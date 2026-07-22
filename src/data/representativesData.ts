2364e9c0038fd48a813fb79e54b14d041d71bee7
ritchiero <appletreemx@gmail.com>
2026-07-21 21:28:16 -0700

    Actualiza actividad verificada (3801 representantes)

--- src/data/representativesData.ts
+++ src/data/representativesData.ts
@@ -30,7 +30,7 @@ export const representativesData: { name: string; brandCount: number; rank: numb
   { rank: 29, name: "Angelica Lara Maldonado", brandCount: 1649 },
   { rank: 30, name: "Gilberto Martinez Maldonado", brandCount: 9221 },
   { rank: 31, name: "Bernardo Herrerias Franco", brandCount: 12170 },
-  { rank: 32, name: "Agustin Manuel Velazquez Garcia-lopez", brandCount: 8395 },
+  { rank: 32, name: "Agustin Manuel Velazquez Garcia-lopez", brandCount: 5648 },
   { rank: 33, name: "Alan Guevara Morales", brandCount: 5450 },
   { rank: 34, name: "Eduardo Birman Ripstein", brandCount: 9884 },
   { rank: 35, name: "Hector Carlos Ancona Torres", brandCount: 7278 },
